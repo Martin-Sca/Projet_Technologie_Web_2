a1297c959d2d84d0aeed19b74173910952cf72f3
Martin Scarnière <martin.scarniere@gmail.com>
2025-01-05 22:34:49 +0100

    Développement d'un modèle

---
+++ src/models/pilotes.ts
@@ -0,0 +1,9 @@
+export class Pilote {
+    constructor(
+      public nom: string,
+      public age: number,
+      public nationalite: string,
+      public ecurie: string,
+      public victoires: number
+    ) {}
+}  
