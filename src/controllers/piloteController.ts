117af7bdaf09bcb56a990afb3853f0e0389cd5b3
Martin Scarnière <martin.scarniere@gmail.com>
2025-01-05 22:37:24 +0100

    Développement du controleur

---
+++ src/controllers/piloteController.ts
@@ -0,0 +1,25 @@
+import { Pilote } from "../models/pilotes";
+import * as fs from "fs";
+
+export class PiloteController {
+  static chargerPilotes(): Pilote[] {
+    const data = fs.readFileSync("src/data/pilotes.json", "utf-8");
+    const pilotes = JSON.parse(data);
+    return pilotes.map((p: any) =>
+      new Pilote(p.nom, p.age, p.nationalite, p.ecurie, p.victoires)
+    );
+  }
+
+  static ajouterPilote(pilotes: Pilote[], nouveauPilote: Pilote): Pilote[] {
+    pilotes.push(nouveauPilote);
+    return pilotes;
+  }
+
+  static trierParAge(pilotes: Pilote[]): Pilote[] {
+    return pilotes.sort((a, b) => a.age - b.age);
+  }
+
+  static trierParVictoires(pilotes: Pilote[]): Pilote[] {
+    return pilotes.sort((a, b) => b.victoires - a.victoires);
+  }
+}
